6096872d331ff63771a7ec79214b92991bb7fa27
Michael Lee <michaellee2245@gmail.com>
2018-11-29 18:31:12 -0700

    mission component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import Loading from './components/Loading/Loading';
 import Apollo from './components/Apollo/Apollo';
+import { Motion, spring } from 'react-motion';
 
 
 
@@ -10,10 +11,10 @@ class App extends Component {
 
   state = {
     loading: true,
-    percentage:0
+    percentage: 0
   };
 
-  componentDidMount(){
+  componentDidMount() {
     // let config = {
     //   onDownloadProgress: progressEvent => {
     //     let percentCompleted = Math.floor((progressEvent.loaded * 100) / 387003 );
@@ -26,23 +27,27 @@ class App extends Component {
     // }
     // axios.get('https://pokeapi.co/api/v2/pokemon/1/', config)
     // .then(res => console.log(res))
-    setTimeout(() => this.setState({loading: false}), 2000)
+    setTimeout(() => this.setState({ loading: false }), 2000)
   }
 
 
 
   render() {
-    
+
     const { loading } = this.state;
 
-    if(loading){
+    if (loading) {
       return <Loading />  //percentage={this.state.percentage}
     }
 
     return (
-      <div className="App">
-        <Apollo />
-      </div>
+      <Motion defaultStyle={{ x: 0 }} style={{ x: spring(10) }}>
+        {interpolatingStyle => (
+          <div className="App">
+            <Apollo />
+          </div>
+        )}
+      </Motion>
     );
   }
 }
---
+++ src/components/Missions/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './missions.css';
+
+class Missions extends Component {
+    render(){
+        return(
+            <div>Missions</div>
+        )
+    }
+}
+
+export default Missions;
