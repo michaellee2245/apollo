679d6014cbb502b000cc8f68277c8caca5a1012a
Michael Lee <michaellee2245@gmail.com>
2019-01-08 18:33:13 -0700

    comments sections working and npm run build succesful

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import './missions.scss';
+import './missions.css';
 
 import ApolloMissionTiles from '../ApolloMissionTiles/ApolloMissionTiles';
 class Missions extends Component {
